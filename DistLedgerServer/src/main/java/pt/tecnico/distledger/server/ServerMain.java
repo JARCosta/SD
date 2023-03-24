5fb8776d699203086c5f4bedd5797c93edcec7df
José Maria <jose.vilar.gomes@tecnico.ulisboa.pt>
2023-03-24 22:19:19 +0000

    Catch signal waits indefinetly for confirmation

--- DistLedgerServer/src/main/java/pt/tecnico/distledger/server/ServerMain.java
+++ DistLedgerServer/src/main/java/pt/tecnico/distledger/server/ServerMain.java
@@ -1,13 +1,13 @@
 package pt.tecnico.distledger.server;
 
 import java.io.IOException;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.CountDownLatch;
 
 import io.grpc.BindableService;
-import io.grpc.Server;
-import io.grpc.ServerBuilder;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
 
 import pt.tecnico.distledger.server.domain.AdminServiceImpl;
 import pt.tecnico.distledger.server.domain.ServerState;
@@ -17,57 +17,62 @@ import pt.ulisboa.tecnico.distledger.contract.distledgerserver.DistLedgerCrossSe
 
 public class ServerMain {
 
-    private final static String host = "localhost";
-    private final static String serviceName = "DistLedgerServerService";
+	private final static String host = "localhost";
+	private final static String serviceName = "DistLedgerServerService";
 
-    public static void main(String[] args) throws IOException, InterruptedException {
+	public static void main(String[] args) throws IOException, InterruptedException {
 
-        System.out.printf("Received %d arguments%n", args.length);
-        for (int i = 0; i < args.length; i++) {
-            System.out.printf("arg[%d] = %s%n", i, args[i]);
-        }
+		System.out.printf("Received %d arguments%n", args.length);
+		for (int i = 0; i < args.length; i++) {
+			System.out.printf("arg[%d] = %s%n", i, args[i]);
+		}
 
-        final String port = args[0];
-        final String qualifier = args[1];
-        final String address = host + ":" + port;
+		final String port = args[0];
+		final String qualifier = args[1];
+		final String address = host + ":" + port;
 
-        // Register server in naming server
-        NamingServerService namingServerService = new NamingServerService();
-        namingServerService.register(serviceName, qualifier, address);
+		// Register server in naming server
+		NamingServerService namingServerService = new NamingServerService();
+		namingServerService.register(serviceName, qualifier, address);
 
-        ServerState ledger;
-        // Create new server state
-        if(qualifier.equals("A")){
-            ledger = new ServerState(namingServerService, serviceName, qualifier);
-        } else {
-            ledger = new ServerState();
-        }
+		ServerState ledger;
+		// Create new server state
+		if (qualifier.equals("A")) {
+			ledger = new ServerState(namingServerService, serviceName, qualifier);
+		} else {
+			ledger = new ServerState();
+		}
 
-        final BindableService userService = new UserServiceImpl(ledger);
-        final BindableService adminService = new AdminServiceImpl(ledger);
+		final BindableService userService = new UserServiceImpl(ledger);
+		final BindableService adminService = new AdminServiceImpl(ledger);
 
-        // Create a new server to listen on port
-        Server server = ServerBuilder.forPort(Integer.parseInt(port))
-                .addService(userService)
-                .addService(adminService)
-                .build();
+		// Create a new server to listen on port
+		Server server = ServerBuilder.forPort(Integer.parseInt(port)).addService(userService).addService(adminService)
+				.build();
 
-        server.start();
-        System.out.println("Server started");
+		server.start();
+		System.out.println("Server started");
 
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            System.out.println("Press enter to shutdown.");
-            try {
-                System.in.read();
-                namingServerService.delete(serviceName, address);
-                namingServerService.shutdownNowChannel();
-            } catch (IOException e) {
-                System.err.println(e.getMessage());
-            }
-        }));
+		Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+			System.out.println("Press enter to confirm shutdown");
+			try {
+				System.in.read();
+				// Delete server from naming server
+				namingServerService.delete(serviceName, address);
+				namingServerService.shutdownNowChannel();
+				server.shutdown();
+				server.awaitTermination();
+			} catch (IOException | InterruptedException e) {
+				System.err.println(e.getMessage());
+			}
+		}));
+	
+		// Wait for termination signal
+		try {
+			Thread.currentThread().join();
+		} catch (InterruptedException e) {
+			System.err.println(e.getMessage());
+		}
+	}
 
-
-        server.shutdown();
-        server.awaitTermination();
-    }
 }
