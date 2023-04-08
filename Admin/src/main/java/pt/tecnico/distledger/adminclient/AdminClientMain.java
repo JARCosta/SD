b6806252393102a69240c4519b6bd726650408f0
João Bettencourt <joao.pais.bettencourt@tecnico.ulisboa.pt>
2023-04-08 11:08:18 +0100

    removed used code in AdminClientMain

--- Admin/src/main/java/pt/tecnico/distledger/adminclient/AdminClientMain.java
+++ Admin/src/main/java/pt/tecnico/distledger/adminclient/AdminClientMain.java
@@ -2,22 +2,10 @@ package pt.tecnico.distledger.adminclient;
 
 
 public class AdminClientMain {
+    
     public static void main(String[] args) {
 
         System.out.println(AdminClientMain.class.getSimpleName());
-
-        // receive and print arguments
-        System.out.printf("Received %d arguments%n", args.length);
-        for (int i = 0; i < args.length; i++) {
-            System.out.printf("arg[%d] = %s%n", i, args[i]);
-        }
-
-        // check arguments
-        if (args.length != 2) {
-            System.err.println("Argument(s) missing!");
-            System.err.println("Usage: mvn exec:java -Dexec.args=<host> <port>");
-            return;
-        }
 
         CommandParser parser = new CommandParser();
         parser.parseInput();
